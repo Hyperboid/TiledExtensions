053a65b1d82f569205b12e2ecacaea16a4878147
hyperboid <23138363+polypoyo@users.noreply.github.com>
2025-02-22 17:20:33 -0500

    Extract addKristalLayersTo method

--- KristalLayers.js
+++ KristalLayers.js
@@ -1,11 +1,6 @@
 // @ts-check
 
-let action = tiled.registerAction("CreateKristalLayers", function(action) {
-    const asset = tiled.activeAsset
-    if (!asset || !asset.isTileMap) {
-        tiled.alert("Not a tile map!"); return;
-    } /* @ts-ignore */
-    /** @type {TileMap} */ const map = asset
+function addKristalLayersTo(/** @type {TileMap} */map) {
     const tryAddLayer = function (/** @type {string} */ name, /** @type {string} */ color) {
         for (let i = 0; i < map.layers.length; i++) {
             const layer = map.layers[i];
@@ -20,6 +15,16 @@ let action = tiled.registerAction("CreateKristalLayers", function(action) {
     tryAddLayer("controllers", "#00C000")
     tryAddLayer("markers", "#7F00FF")
     tryAddLayer("battleareas", "#00FF00")
+}
+
+let action = tiled.registerAction("CreateKristalLayers", function(action) {
+    const asset = tiled.activeAsset
+    if (!asset || !asset.isTileMap) {
+        tiled.alert("Not a tile map!"); return;
+    }
+    // @ts-ignore
+    addKristalLayersTo(asset)
+    
 })
 
 action.shortcut = "Ctrl+K"
